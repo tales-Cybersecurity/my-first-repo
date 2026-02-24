9f3bb768b2d8f78c6a91af176ba72b32c2bbff62
tales-Cybersecurity <tales.lima.paula@gamil.com>
2026-02-24 10:06:52 +0000

    fix task 3

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 99:
-        print(f"{i:02d}", end=", ")
-    else:
-        print(f"{i:02d}")
+    print("%02d" % i, end=", " if i < 99 else "\n")
